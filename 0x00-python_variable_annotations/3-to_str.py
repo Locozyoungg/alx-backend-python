ff804255a429d72816e8d568c9b25c37da1c61a9
Locozyoungg <collaustine27@gmail.com>
2024-07-05 00:46:28 +0300

    to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module for to_str function
+"""
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the float.
+
+    Args:
+    n (float): The number to convert.
+
+    Returns:
+    str: The string representation of the number.
+    """
+    return str(n)
+
