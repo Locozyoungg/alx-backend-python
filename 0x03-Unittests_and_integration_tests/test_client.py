acf08be9eb33c28efdef85fc13d06c9a470980e7
Locozyoungg <collaustine27@gmail.com>
2024-07-30 01:37:39 +0300

    test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -17,5 +17,25 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, {"name": "mocked_org"})
 
+    @parameterized.expand([
+        ("google", "https://api.github.com/orgs/google/repos"),
+        ("abc", "https://api.github.com/orgs/abc/repos")
+    ])
+    @patch('client.GithubOrgClient.org', new_callable=property)
+    def test_public_repos_url(self, org_name, expected_url, mock_org):
+        # Define the mock payload
+        mock_org.return_value = {
+            'repos_url': expected_url
+        }
+        
+        # Instantiate the client
+        client = GithubOrgClient(org_name)
+        
+        # Call the method
+        result = client._public_repos_url
+        
+        # Assert the result
+        self.assertEqual(result, expected_url)
+
 if __name__ == '__main__':
     unittest.main()
