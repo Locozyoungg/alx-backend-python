582087078cdfec4b88dbdff957ba33d8b6c07b9c
Locozyoungg <collaustine27@gmail.com>
2024-07-30 01:28:37 +0300

    test_client

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+class TestGithubOrgClient(unittest.TestCase):
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",)
+    ])
+    @patch('client.GithubOrgClient.get_json', return_value={"name": "mocked_org"})
+    def test_org(self, org_name, mock_get_json):
+        client = GithubOrgClient(org_name)
+        result = client.org
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        self.assertEqual(result, {"name": "mocked_org"})
+
+if __name__ == '__main__':
+    unittest.main()
